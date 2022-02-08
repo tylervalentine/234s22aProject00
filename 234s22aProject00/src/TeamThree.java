cb66bd547d78219f9a82606d7cbf7e5b7cc7f564
bryan <cohenb@moravian.edu>
2022-02-08 09:55:01 -0500

    first commit

--- 234s22aProject00/src/TeamThree.java
+++ 234s22aProject00/src/TeamThree.java
@@ -1,29 +1,22 @@
-<<<<<<< HEAD
 
 
-public class TeamThree extends Team{
-    public TeamThree(String name){
-=======
 public class TeamThree extends Team
 {
     public TeamThree(String name)
     {
->>>>>>> origin/main
+
         super(name);
         createTeamMembers();
     }
 
-<<<<<<< HEAD
-    public void createTeamMembers(){
-        members.add(new TeamMember("Bryan Cohen"));
-=======
     public void createTeamMembers()
     {
         members.add(new TeamMember("Tyler Valentine"));
         members.add(new TeamMember("Reed Sturza"));
         members.add(new TeamMember("Fred Flintstone"));
+        members.add(new TeamMember("Bryan Cohen"));
         // create a new member class for each team member
         // and add it to the team members list
->>>>>>> origin/main
+
     }
 }
